41383f9308eb7bd091cde3ede3022ae836325da3
EvilMoOd <962475158@qq.com>
2023-03-24 16:57:34 +0800

    fix: path

--- docs/.vitepress/config/plugins.ts
+++ docs/.vitepress/config/plugins.ts
@@ -21,7 +21,7 @@ export const mdPlugin = (md) => {
         const sourceFile = sourceFileToken.children?.[0].content ?? '';
         if (sourceFileToken.type === 'inline') {
           source = fs.readFileSync(
-            resolve(__dirname, '../../demos', `${sourceFile}.vue`),
+            resolve('./docs/demos', `${sourceFile}.vue`),
             'utf-8'
           );
         }
