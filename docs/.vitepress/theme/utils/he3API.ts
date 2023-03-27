57b922ac192edfc2f0077fe6f87aae03f54d5bcd
EvilMoOd <962475158@qq.com>
2023-03-27 17:03:37 +0800

    fix: window

--- docs/.vitepress/theme/utils/he3API.ts
+++ docs/.vitepress/theme/utils/he3API.ts
@@ -1,11 +1,13 @@
 export function setHe3() {
-  window.$he3 = {};
-  window.$he3.message = () => {};
-  window.$he3.getTheme = () => {};
-  window.$he3.subscribeThemeChange = () => {};
-  window.$he3.getLastClipboard = () => {
-    return new Promise((resolve) => {
-      resolve(1);
-    });
-  };
+  if (window) {
+    window.$he3 = {};
+    window.$he3.message = () => {};
+    window.$he3.getTheme = () => {};
+    window.$he3.subscribeThemeChange = () => {};
+    window.$he3.getLastClipboard = () => {
+      return new Promise((resolve) => {
+        resolve(1);
+      });
+    };
+  }
 }
--- docs/demos/transform/basic.vue
+++ docs/demos/transform/basic.vue
@@ -1,6 +1,6 @@
 <template>
   <div style="height: 500px">
-    <h-transform :onChange="handleChange" :onMounted="handleMounted" />
+    <h-transform  :onChange="handleChange" :onMounted="handleMounted" />
   </div>
 </template>
 
