77ce5315c18bf165d139fdb3f85d9c0bbadc94b4
EvilMoOd <962475158@qq.com>
2023-03-27 17:06:23 +0800

    fix: window

--- docs/.vitepress/theme/utils/he3API.ts
+++ docs/.vitepress/theme/utils/he3API.ts
@@ -1,13 +1,12 @@
 export function setHe3() {
-  if (window) {
-    window.$he3 = {};
-    window.$he3.message = () => {};
-    window.$he3.getTheme = () => {};
-    window.$he3.subscribeThemeChange = () => {};
-    window.$he3.getLastClipboard = () => {
-      return new Promise((resolve) => {
-        resolve(1);
-      });
-    };
-  }
+  if (typeof window === 'undefined') return;
+  window.$he3 = {};
+  window.$he3.message = () => {};
+  window.$he3.getTheme = () => {};
+  window.$he3.subscribeThemeChange = () => {};
+  window.$he3.getLastClipboard = () => {
+    return new Promise((resolve) => {
+      resolve(1);
+    });
+  };
 }
